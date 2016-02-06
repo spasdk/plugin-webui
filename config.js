740141716a743840f52d6a2026a19eb00ade0df3
DarkPark <darkpark@pisem.net>
2016-02-06 19:52:37 +0200

    clone static plugin

--- config.js
+++ config.js
@@ -5,14 +5,37 @@
 
 'use strict';
 
-var extend   = require('extend'),
+var path     = require('path'),
+    extend   = require('extend'),
     config   = require('spasdk/config'),
     profiles = {};
 
+//console.log(require.resolve('spa-plugin-webui'));
+//console.log(path.join(__dirname, 'app'));
 
 // main
 profiles.default = extend(true, {}, config, {
+    // directory to serve
+    source: path.join(__dirname, 'app'),
 
+    // main entry point to load web page
+    target: 'index.html',
+
+    // listening port (0 - random)
+    port: 8000,
+
+    // static file server cache activation
+    // false to disable or amount of seconds to cache
+    cache: 3600,
+
+    // info channels
+    notifications: {
+        popup: {
+            info: {icon: path.join(__dirname, 'media', 'info.png')},
+            warn: {icon: path.join(__dirname, 'media', 'warn.png')},
+            fail: {icon: path.join(__dirname, 'media', 'fail.png')}
+        }
+    }
 });
 
 
--- index.js
+++ index.js
@@ -5,17 +5,110 @@
 
 'use strict';
 
-var Plugin   = require('spasdk/lib/plugin'),
-    plugin   = new Plugin({name: 'webui', entry: 'serve', config: require('./config')});
+var http   = require('http'),
+    path   = require('path'),
+    util   = require('util'),
+    open   = require('open'),
+    Plugin = require('spasdk/lib/plugin'),
+    plugin = new Plugin({name: 'webui', entry: 'serve', config: require('./config')}),
+    ip     = require('ip').address();
+
+
+// rework profile
+//plugin.prepare = function ( name ) {
+//    var profile = this.config[name];
+//
+//    profile.target = profile.target.replace(/\$\{host}/g, ip);
+//    profile.target = profile.target.replace(/\$\{port}/g, profile.port);
+//};
 
 
 // create tasks for profiles
-//plugin.profiles.forEach(function ( profile ) {
-//    // main entry task
-//    profile.task(plugin.entry, function ( done ) {
-//
-//    });
-//});
+plugin.profiles.forEach(function ( profile ) {
+    var srcDir = path.resolve(profile.data.source),
+        server;
+
+    // correct target
+    //plugin.prepare(profile.name);
+
+    // main entry task
+    profile.task(plugin.entry, function ( done ) {
+        // rfc 2616 compliant HTTP static file server
+        var files = new (require('node-static').Server)(profile.data.source, {cache: profile.data.cache});
+
+        server = http.createServer(function ( request, response ) {
+            request.addListener('end', function () {
+                // static files
+                files.serve(request, response, function serve ( error ) {
+                    var address = request.connection.remoteAddress || '[0.0.0.0]'.red,
+                        status  = response.statusCode === 200 ? response.statusCode.toString().green : response.statusCode.toString().yellow;
+
+                    if ( error ) {
+                        response.end();
+                    }
+
+                    // single file serving report
+                    profile.notify({
+                        type: error ? 'fail' : 'info',
+                        info: [
+                            address.replace('::ffff:', '').replace(/\./g, '.'.grey),
+                            (+new Date()).toString().substr(-3).grey,
+                            error ? error.status.toString().red : status,
+                            request.method.grey,
+                            request.url.replace(/\//g, '/'.grey)
+                        ].join('\t'),
+                        title: plugin.entry,
+                        message: error ? [request.url, '', error.message] : request.url
+                    });
+                });
+            }).resume();
+        });
+
+        server.on('listening', function () {
+            // port can be 0 from the start
+            profile.data.port = server.address().port;
+
+            // report
+            profile.notify({
+                info: 'serve '.green + srcDir.bold + ' with entry '.green + util.format('http://%s:%s/%s', ip, profile.data.port, profile.data.target).blue,
+                title: plugin.entry,
+                message: util.format('serve %s\non port %s', srcDir, ip + ':' + profile.data.port)
+            });
+        });
+
+        server.on('close', done);
+
+        server.on('error', function ( error ) {
+            profile.notify({
+                type: 'fail',
+                info: error.message,
+                title: plugin.entry,
+                message: error.message
+            });
+
+            done();
+        });
+
+        server.listen(profile.data.port);
+    });
+
+    // open page in browser
+    profile.task('open', function () {
+        open(profile.data.target);
+    });
+
+    profile.task('stop', function () {
+        if ( server ) {
+            profile.notify({
+                info: 'stop '.green + srcDir.bold,
+                title: 'stop',
+                message: 'stop ' + srcDir
+            });
+
+            server.close();
+        }
+    });
+});
 
 
 // public
