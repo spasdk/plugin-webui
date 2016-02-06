14868e5fc94107ac726b91e9d5036c0720ad8820
DarkPark <darkpark@pisem.net>
2016-02-06 19:53:42 +0200

    build list of task buttons

--- src/js/main.js
+++ src/js/main.js
@@ -5,7 +5,26 @@
 'use strict';
 
 var app    = require('spa-app'),
+    Wamp   = require('cjs-wamp'),
+    parse  = require('cjs-parse-query'),
     router = require('spa-router');
+
+
+function wamp () {
+    app.wamp = new Wamp(new WebSocket('ws://localhost:' + parse(document.location.search.substring(1)).port + '/client'));
+
+    // ready
+    app.wamp.socket.onopen = function () {
+        console.log('wamp is ready!');
+    };
+
+    // try to reconnect in 5 seconds on disconnect
+    app.wamp.socket.onclose = function () {
+        setTimeout(wamp, 5000);
+    };
+
+    return app.wamp;
+}
 
 
 // main application events
@@ -21,7 +40,25 @@ app.addListeners({
 
     // everything is ready
     done: function done () {
-        // go to the main page when necessary
-        router.navigate('pageMain');
+        wamp();
+
+        // ready
+        app.wamp.socket.onopen = function () {
+            app.wamp.call('getClients', {}, function ( error, data ) {
+                console.log('clients', data);
+            });
+
+            app.wamp.call('getTargets', {}, function ( error, data ) {
+                console.log('targets', data);
+            });
+
+            app.wamp.call('getPlugins', {}, function ( error, data ) {
+                console.log('plugins', data);
+            });
+
+
+            // go to the main page when necessary
+            router.navigate('pageMain');
+        };
     }
 });
--- src/js/pages/main.js
+++ src/js/pages/main.js
@@ -4,8 +4,30 @@
 
 'use strict';
 
-var Page = require('spa-component-page'),
+var app  = require('spa-app'),
+    Page = require('spa-component-page'),
     page = new Page({$node: window.pageMain});
+
+
+page.addListener('show', function load () {
+    app.wamp.call('getTasks', {}, function ( error, data ) {
+        console.log('tasks', data);
+
+        data.forEach(function ( id ) {
+            var div = document.createElement('div');
+
+            div.innerText = id;
+            div.className = 'button';
+            div.addEventListener('click', function () {
+                app.wamp.call('runTask', {id: div.innerText}, function ( error, data ) {
+                    console.log('run task', div.innerText, data);
+                });
+            });
+
+            page.$body.appendChild(div);
+        });
+    });
+});
 
 
 // public
